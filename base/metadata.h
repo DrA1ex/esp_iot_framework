ab33a495cefc8ea6c2eef75ded332fecf79c9820
Alexander <DrA1ex@users.noreply.github.com>
2024-09-26 15:02:18 +0500

    Switch to new codestyles

--- base/metadata.h
+++ base/metadata.h
@@ -43,15 +43,15 @@ struct PropertyMeta : AbstractPropertyMeta {
     PropertyMeta(ParameterT &&parameter) : parameter(std::move(parameter)) {} // NOLINT(*-explicit-constructor)
 
     PropertyMeta(PacketEnumT packet_type, ParameterT &&parameter)
-            : parameter(std::move(parameter)), binary_protocol{packet_type} {}
+        : parameter(std::move(parameter)), binary_protocol{packet_type} {}
 
     PropertyMeta(const char *topic_in, const char *topic_out, ParameterT &&parameter)
-            : parameter(std::move(parameter)), mqtt_protocol{topic_in, topic_out} {}
+        : parameter(std::move(parameter)), mqtt_protocol{topic_in, topic_out} {}
     PropertyMeta(const char *topic_out, ParameterT &&parameter)
-            : parameter(std::move(parameter)), mqtt_protocol{nullptr, topic_out} {}
+        : parameter(std::move(parameter)), mqtt_protocol{nullptr, topic_out} {}
 
     PropertyMeta(PacketEnumT packet_type, const char *topic_in, const char *topic_out, ParameterT &&parameter)
-            : parameter(std::move(parameter)), binary_protocol{packet_type}, mqtt_protocol{topic_in, topic_out} {}
+        : parameter(std::move(parameter)), binary_protocol{packet_type}, mqtt_protocol{topic_in, topic_out} {}
     PropertyMeta(PacketEnumT packet_type, const char *topic_out, ParameterT &&parameter)
-            : parameter(std::move(parameter)), binary_protocol{packet_type}, mqtt_protocol{nullptr, topic_out} {}
+        : parameter(std::move(parameter)), binary_protocol{packet_type}, mqtt_protocol{nullptr, topic_out} {}
 };
