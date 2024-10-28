017fa51daaf70b4090589b360c62ac50bdf14796
Alexander <DrA1ex@users.noreply.github.com>
2024-10-28 16:20:01 +0500

    Improve Button implementation

--- misc/button.cpp
+++ misc/button.cpp
@@ -1,50 +1,86 @@
 #include "button.h"
 
+#include <esp_sleep.h>
+
 #include "../debug.h"
 
-Button::Button(uint8_t pin, bool high_state) : _pin(pin), _high_state(high_state) {}
+Button::Button(uint8_t pin, bool high_state, bool used_for_wakeup) : _pin(pin), _high_state(high_state),
+                                                                     _used_for_wakeup(used_for_wakeup) {}
 
-void Button::begin() {
-    pinMode(_pin, INPUT);
+void Button::begin(uint8_t mode) {
+    pinMode(_pin, mode);
 
-    attachInterruptArg(digitalPinToInterrupt(_pin), _handle_interrupt, this, CHANGE);
+#ifdef SOC_GPIO_SUPPORT_DEEPSLEEP_WAKEUP
+    if (_used_for_wakeup) {
+        auto wakeup_status = esp_sleep_get_gpio_wakeup_status();
+        bool waked_up = wakeup_status & (1 << _pin);
+        if (!_read() && waked_up) {
+            _last_impulse_time = millis();
+            _click_count = 1;
+        }
+    }
+#endif
+
+    _last_interrupt_state = _read();
+    attachInterruptArg(digitalPinToInterrupt(_pin), _handle_interrupt_change_static, this, CHANGE);
 
     D_PRINTF("Setup button interruption for pin %u\n", _pin);
+}
+
+void Button::end() {
+    detachInterrupt(digitalPinToInterrupt(_pin));
+    pinMode(_pin, INPUT);
+
+    _click_count = 0;
+    _hold = false;
+
+    D_PRINTF("Detach button interruption for pin %u\n", _pin);
 }
 
 bool Button::_read() const {
     return digitalRead(_pin) ^ !_high_state;
 }
 
-void Button::_handle_interrupt(void *arg) {
+void Button::_handle_interrupt_change_static(void *arg) {
     auto self = (Button *) arg;
-    self->_handle_interrupt();
+    self->_handle_interrupt_change();
 }
 
-void Button::_handle_interrupt() {
-    auto silence_time = millis() - _last_impulse_time;
+void Button::_handle_interrupt_change() {
+    auto delta = millis() - _last_impulse_time;
     _last_impulse_time = millis();
 
-    if (silence_time < BTN_SILENCE_INTERVAL) return;
+    if (delta < BTN_SILENCE_INTERVAL) {
+        VERBOSE(D_PRINT("Button: filtering noise"));
+        return;
+    }
 
-    if (_read()) {
-        _handle_rising_interrupt();
+    bool state = _read();
+    if (state == _last_interrupt_state) {
+        VERBOSE(D_PRINTF("Button interruption: Got same state (%i). Inverting\n", state));
+        state = !state;
+    }
+
+    _last_interrupt_state = state;
+    if (state) {
+        _handle_rising_interrupt(delta);
     } else {
-        _handle_falling_interrupt();
+        _handle_falling_interrupt(delta);
     }
 }
 
 
-void Button::_handle_rising_interrupt() {
-    const auto delta = millis() - _last_impulse_time;
-    if (delta > BTN_RESET_INTERVAL) {
-        VERBOSE(D_PRINT("Button Interception: Start Over"));
+void Button::_handle_rising_interrupt(unsigned long delta) {
+    VERBOSE(D_PRINT("Button Interception: RISING"));
+    if ((_click_count || _hold) && delta > BTN_RESET_INTERVAL) {
+        VERBOSE(D_PRINT("Button Interception: Start Over. Forget to call Button::handle() ?"));
+        _hold = false;
         _click_count = 0;
     }
 }
 
-
-void Button::_handle_falling_interrupt() {
+void Button::_handle_falling_interrupt(unsigned long delta) {
+    VERBOSE(D_PRINT("Button Interception: FALLING"));
     if (!_hold) {
         VERBOSE(D_PRINT("Button Interception: Click"));
         _click_count++;
@@ -60,9 +96,6 @@ void Button::handle() {
         VERBOSE(D_PRINT("Button: Set Hold"));
         _hold = true;
         _click_count++;
-    } else if (_click_count && !_hold && delta >= BTN_RESET_INTERVAL) {
-        VERBOSE(D_PRINT("Button: Reset"));
-        _click_count = 0;
     } else if (_hold && !state) {
         D_PRINT("Button: Button hold release");
 
@@ -72,21 +105,36 @@ void Button::handle() {
 
         _hold = false;
         _click_count = 0;
+        _last_interrupt_state = false;
     }
 
     if (_hold) {
-        D_PRINTF("Button: Hold #%i\n", _click_count);
+        auto hold_call_delta = millis() - _last_button_hold_call_time;
+        if (hold_call_delta >= BTN_HOLD_CALL_INTERVAL) {
+            D_PRINTF("Button: Hold #%i\n", _click_count);
 
-        if (_hold_handler != nullptr) {
-            _hold_handler(_click_count);
+            _last_state.click_count = _click_count;
+            _last_state.hold = true;
+            _last_state.timestamp = millis();
+
+            if (_hold_handler != nullptr) {
+                _hold_handler(_click_count);
+            }
+
+            _last_button_hold_call_time = millis();
         }
     } else if (_click_count && delta > BTN_PRESS_WAIT_INTERVAL) {
         D_PRINTF("Button: Click count %i\n", _click_count);
+
+        _last_state.click_count = _click_count;
+        _last_state.hold = false;
+        _last_state.timestamp = millis();
 
         if (_click_handler != nullptr) {
             _click_handler(_click_count);
         }
 
         _click_count = 0;
+        _last_interrupt_state = false;
     }
 }
--- misc/button.h
+++ misc/button.h
@@ -10,6 +10,10 @@
 #define BTN_HOLD_INTERVAL                       (500u)
 #endif
 
+#ifndef BTN_HOLD_CALL_INTERVAL
+#define BTN_HOLD_CALL_INTERVAL                  (500u)
+#endif
+
 #ifndef BTN_PRESS_WAIT_INTERVAL
 #define BTN_PRESS_WAIT_INTERVAL                 (500u)
 #endif
@@ -22,11 +26,19 @@
 typedef std::function<void(uint8_t count)> ButtonOnClickFn;
 typedef ButtonOnClickFn ButtonOnHoldFn;
 
+struct ButtonState {
+    bool hold = false;
+    uint8_t click_count = 0;
+
+    unsigned long timestamp = 0;
+};
+
 class Button {
     volatile bool _hold = false;
     volatile int _click_count = 0;
 
     volatile unsigned long _last_impulse_time = 0;
+    unsigned long _last_button_hold_call_time = 0;
 
     ButtonOnClickFn _click_handler = nullptr;
     ButtonOnHoldFn _hold_handler = nullptr;
@@ -34,23 +46,33 @@ class Button {
 
     uint8_t _pin;
     bool _high_state;
+    bool _used_for_wakeup;
+
+    bool _last_interrupt_state = false;
+    ButtonState _last_state;
 
 public:
-    explicit Button(uint8_t pin, bool high_state = true);
+    explicit Button(uint8_t pin, bool high_state = true, bool used_for_wakeup = false);
 
-    void begin();
+    void begin(uint8_t mode = INPUT);
     void handle();
+
+    void end();
+
+    [[nodiscard]] inline bool idle() const { return !_hold && _click_count == 0; }
+    [[nodiscard]] inline const ButtonState &last_state() const { return _last_state; }
 
     inline void set_on_click(const ButtonOnClickFn &fn) { _click_handler = fn; }
     inline void set_on_hold(const ButtonOnHoldFn &fn) { _hold_handler = fn; }
     inline void set_on_hold_release(const ButtonOnHoldFn &fn) { _hold_release_handler = fn; }
 
 private:
-    bool _read() const;
+    [[nodiscard]] bool _read() const;
 
-    IRAM_ATTR static void _handle_interrupt(void *arg);
-    IRAM_ATTR void _handle_interrupt();
+    IRAM_ATTR static void _handle_interrupt_change_static(void *arg);
 
-    IRAM_ATTR void _handle_rising_interrupt();
-    IRAM_ATTR void _handle_falling_interrupt();
+    IRAM_ATTR void _handle_interrupt_change();
+
+    IRAM_ATTR void _handle_rising_interrupt(unsigned long delta);
+    IRAM_ATTR void _handle_falling_interrupt(unsigned long delta);
 };
